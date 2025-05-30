bc62aa851b105d36ceb3de9a1b83bfe1f42205aa
jirniyuwu <vvvtvalvl@gmail.com>
2025-05-30 15:05:48 +0300

    initial commit

---
+++ src/main/java/net/jirniy/pinkstuff/JirniysPinkStuff.java
@@ -0,0 +1,16 @@
+package net.jirniy.pinkstuff;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JirniysPinkStuff implements ModInitializer {
+	public static final String MOD_ID = "pinkstuff";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/jirniy/pinkstuff/JirniysPinkStuffClient.java
@@ -0,0 +1,10 @@
+package net.jirniy.pinkstuff;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JirniysPinkStuffClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
